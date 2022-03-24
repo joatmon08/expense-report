933360d50ca2adac7dd5dab21faa4d9ef4b17c05
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-03-24 08:43:54 -0400

    Add OTel

--- expense/dotnet/Program.cs
+++ expense/dotnet/Program.cs
@@ -1,14 +1,52 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.HttpOverrides;
 using expense.Contexts;
 using expense.Models;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
+var serviceName = builder.Configuration.GetValue<string>("Name");
+var serviceVersion = builder.Configuration.GetValue<string>("Version");
+
+var metricsEndpoint = builder.Configuration
+    .GetValue<string>("MetricsEndpoint", "http://localhost:9464/");
+
+var tracingAgentHost = builder.Configuration
+    .GetSection("Jaeger").GetValue<string>("AgentHost", "localhost");
+var tracingAgentPort = builder.Configuration
+    .GetSection("Jaeger").GetValue<int>("AgentPort", 6831);
+
+builder.Services.AddOpenTelemetryMetrics(b =>
 {
-    options.ForwardedHeaders =
-        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    b
+    .AddPrometheusExporter(o =>
+    {
+        o.StartHttpListener = true;
+        o.HttpListenerPrefixes = new string[] { metricsEndpoint };
+    })
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation();
+});
+
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b
+    .AddJaegerExporter(o =>
+    {
+        o.AgentHost = tracingAgentHost;
+        o.AgentPort = tracingAgentPort;
+    })
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    .AddSqlClientInstrumentation(o => {
+        o.SetDbStatementForText = true;
+    })
+    .AddHttpClientInstrumentation()
+    .AddAspNetCoreInstrumentation();
 });
 
 // Add services to the container.
@@ -18,12 +56,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ExpenseDbContext>(opt =>
-    opt.UseSqlServer(
-        builder.Configuration.GetConnectionString("ExpensesDatabase")));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ExpensesDatabase")));
 
 builder.Services.AddScoped<IExpenseContext, ExpenseContext>();
 builder.Services.AddTransient<IVersionContext>(s => new VersionContext(
-    builder.Configuration.GetValue<string>("Version")));
+    serviceVersion));
 
 
 var app = builder.Build();
