49117959f30e19087f3eeb2de33e8a560399d316
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-04-12 09:25:02 -0400

    Add Consul root token output as Kubernetes secret

--- terraform/infrastructure/outputs.tf
+++ terraform/infrastructure/outputs.tf
@@ -23,10 +23,31 @@ output "consul_token" {
   sensitive = true
 }
 
+output "consul_token_kubernetes_secret" {
+  value     = hcp_consul_cluster_root_token.consul.kubernetes_secret
+  sensitive = true
+}
+
 output "vault_cluster_id" {
   value = hcp_vault_cluster.vault.cluster_id
 }
 
+output "vault_private_endpoint" {
+  value = hcp_vault_cluster.vault.vault_private_endpoint_url
+}
+
+output "vault_public_endpoint" {
+  value = hcp_vault_cluster.vault.vault_public_endpoint_url
+}
+
 output "consul_cluster_id" {
   value = hcp_consul_cluster.consul.cluster_id
+}
+
+output "consul_private_endpoint" {
+  value = hcp_consul_cluster.consul.consul_private_endpoint_url
+}
+
+output "consul_public_endpoint" {
+  value = hcp_consul_cluster.consul.consul_public_endpoint_url
 }
