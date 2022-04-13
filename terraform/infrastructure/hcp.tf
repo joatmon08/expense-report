e69901c6a4b26b6a64ed4c99494d745b3e14b549
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-04-13 12:05:44 -0400

    Remove HCP

--- terraform/infrastructure/hcp.tf
+++ terraform/infrastructure/hcp.tf
@@ -1,9 +1,9 @@
-resource "hcp_hvn" "cloud" {
-  hvn_id         = "hvn"
-  cloud_provider = "aws"
-  region         = "us-east-1"
-  cidr_block     = "172.25.16.0/20"
-}
+# resource "hcp_hvn" "cloud" {
+#   hvn_id         = "hvn"
+#   cloud_provider = "aws"
+#   region         = "us-east-1"
+#   cidr_block     = "172.25.16.0/20"
+# }
 
 # resource "hcp_consul_cluster" "consul" {
 #   cluster_id      = var.prefix
@@ -16,13 +16,13 @@ resource "hcp_hvn" "cloud" {
 #   cluster_id = hcp_consul_cluster.consul.cluster_id
 # }
 
-resource "hcp_vault_cluster" "vault" {
-  cluster_id      = var.prefix
-  hvn_id          = hcp_hvn.cloud.hvn_id
-  tier            = "dev"
-  public_endpoint = true
-}
+# resource "hcp_vault_cluster" "vault" {
+#   cluster_id      = var.prefix
+#   hvn_id          = hcp_hvn.cloud.hvn_id
+#   tier            = "dev"
+#   public_endpoint = true
+# }
 
-resource "hcp_vault_cluster_admin_token" "vault" {
-  cluster_id = hcp_vault_cluster.vault.cluster_id
-}
+# resource "hcp_vault_cluster_admin_token" "vault" {
+#   cluster_id = hcp_vault_cluster.vault.cluster_id
+# }
