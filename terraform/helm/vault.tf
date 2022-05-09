42422126c8740703ef4bb745eff26f7123fdfca7
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2022-05-09 08:58:59 -0400

    Add back Vault

--- terraform/helm/vault.tf
+++ terraform/helm/vault.tf
@@ -1,32 +1,32 @@
-# resource "helm_release" "vault" {
-#   depends_on = [
-#     helm_release.consul
-#   ]
-#   name       = "vault"
-#   repository = "https://helm.releases.hashicorp.com"
-#   chart      = "vault"
-#   version    = var.vault_helm_version
+resource "helm_release" "vault" {
+  depends_on = [
+    helm_release.consul
+  ]
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  version    = var.vault_helm_version
 
-#   values = [
-#     templatefile("templates/vault.yaml", {
-#       VAULT_TOKEN = var.vault_token
-#     })
-#   ]
-# }
+  values = [
+    templatefile("templates/vault.yaml", {
+      VAULT_TOKEN = var.vault_token
+    })
+  ]
+}
 
-# resource "kubernetes_manifest" "vault" {
-#   depends_on = [
-#     helm_release.consul
-#   ]
-#   manifest = {
-#     "apiVersion" = "consul.hashicorp.com/v1alpha1"
-#     "kind"       = "ServiceDefaults"
-#     "metadata" = {
-#       "name"      = "vault"
-#       "namespace" = "default"
-#     }
-#     "spec" = {
-#       "protocol" = "tcp"
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "vault" {
+  depends_on = [
+    helm_release.consul
+  ]
+  manifest = {
+    "apiVersion" = "consul.hashicorp.com/v1alpha1"
+    "kind"       = "ServiceDefaults"
+    "metadata" = {
+      "name"      = "vault"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "protocol" = "tcp"
+    }
+  }
+}
